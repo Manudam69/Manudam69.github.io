99f011df40782d42e6c5e4c38845c58a426c4e21
Manuel Alonso <malonso@gobdigital.com>
2024-04-05 16:29:39 -0600

    Se crea pantalla de contruccion

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://manudam69.github.io/Manudam69/",
-  base: "main",
   integrations: [tailwind()],
 });
