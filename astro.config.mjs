bdb17896edcc693c40b5299c5748b93e4e1d69c9
Manuel Alonso <malonso@gobdigital.com>
2024-04-05 16:49:27 -0600

    Se modifica site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://manudam69.github.io/Manudam69/",
+  site: "https://manudam69.github.io",
   integrations: [tailwind()],
 });
