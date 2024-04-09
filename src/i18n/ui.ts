3be330424066c090026143045c8209d91fadb6dc
Manuel Alonso <malonso@gobdigital.com>
2024-04-08 18:24:41 -0600

    Configuracion i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "header.profession": "Desarrollador Web | Frontend y Backend",
+    "header.description":
+      "Soy un apasionado ingeniero en sistemas especializado en el desarrollo web full-stack. Mi experiencia abarca tanto el front-end como el back-end. En el ámbito del front-end, he trabajado con frameworks como React, Angular y JQuery, creando interfaces de usuario atractivas e intuitivas que mejoran la experiencia del usuario.",
+    "header.contacto": "Información de contacto",
+    "experience.experience": "Experiencia",
+    "experience.skills": "Habilidades",
+    "experience.languaje": "Idiomas",
+    "experience.projects": "Proyectos",
+    "experience.education": "Educación",
+
+  },
+  en: {
+    "header.profession": "Web Developer | Frontend & Backend",
+    "header.description":
+      "I am a passionate Systems Engineer specializing in full-stack web development. My expertise spans both front-end and back-end technologies, utilizing industry-leading tools.",
+    "header.contacto": "Contact information",
+    "experience.experience": "Experience",
+    "experience.skills": "Skills",
+    "experience.languaje": "Languages",
+    "experience.projects": "Projects",
+    "experience.education": "Education",
+  },
+} as const;
