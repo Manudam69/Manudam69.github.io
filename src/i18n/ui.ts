071184547a94c036cec2bcca88617089fe4b0c1e
malonso <manueldavid@live.com.mx>
2025-01-15 10:19:30 -0600

    Se modifica descripcion

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,10 +8,10 @@ export const showDefaultLang = false;
 
 export const routes = {
   es: {
-    'inicio': 'inicio'
+    inicio: "inicio",
   },
   en: {
-    'inicio': 'home'
+    inicio: "home",
   },
 };
 
@@ -19,7 +19,7 @@ export const ui = {
   es: {
     "header.profession": "Desarrollador Web | Frontend y Backend",
     "header.description":
-      "Soy un apasionado ingeniero en sistemas especializado en el desarrollo web full-stack. Mi experiencia abarca tanto el front-end como el back-end. En el ámbito del front-end, he trabajado con frameworks como React, Angular y JQuery, creando interfaces de usuario atractivas e intuitivas que mejoran la experiencia del usuario.",
+      "Desarrollador auto-motivado con más de 4 años de experiencia colaborando y trabajando en múltiples proyectos basados en la web. Programador apasionado y trabajador, con inclinación por desarrollar interfaces personalizadas que consideren demandas únicas de accesibilidad, alcance y seguridad. Enfocado en la organización para cumplir con múltiples plazos concurrentes. Aplica un conocimiento activo del panorama tecnológico actual para fomentar las mejores prácticas en el diseño web.",
     "header.contacto": "Información de contacto",
     "experience.experience": "Experiencia",
     "experience.skills": "Habilidades",
@@ -28,12 +28,12 @@ export const ui = {
     "experience.education": "Educación",
     "experience.present": "Presente",
     "projects.technologies": "Tecnologías utlilizadas",
-    "projects.description": "Descripción"
+    "projects.description": "Descripción",
   },
   en: {
     "header.profession": "Web Developer | Frontend & Backend",
     "header.description":
-      "I am a passionate Systems Engineer specializing in full-stack web development. My expertise spans both front-end and back-end technologies, utilizing industry-leading tools.",
+      "Self-motivated Developer adds high level of experience over more than 4 years collaborating and working on multiple web-based projects. Passionate, hardworking coder with penchant for developing customized interfaces that factor in unique demands for accessibility, reachability and security. Organized approach to meeting multiple, concurrent deadlines. Pulls from active knowledge of current technology landscape to promote best practices in web design.",
     "header.contacto": "Contact information",
     "experience.experience": "Experience",
     "experience.skills": "Skills",
@@ -42,6 +42,6 @@ export const ui = {
     "experience.education": "Education",
     "experience.present": "Present",
     "projects.technologies": "Technologies Used",
-    "projects.description": "Description"
+    "projects.description": "Description",
   },
 } as const;
